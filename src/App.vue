6bf793b36691c56af708a4e3695fb9dbfe46f5f7
urugus-adamah <zc73s.masuwari@gmail.com>
2021-05-10 07:09:50 +0900

    Add Mock data

--- src/App.vue
+++ src/App.vue
@@ -9,24 +9,106 @@
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+html, body, div, span, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+abbr, address, cite, code,
+del, dfn, em, img, ins, kbd, q, samp,
+small, strong, sub, sup, var,
+b, i,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, figcaption, figure,
+footer, header, hgroup, menu, nav, section, summary,
+time, mark, audio, video {
+    margin:0;
+    padding:0;
+    border:0;
+    outline:0;
+    font-size:100%;
+    vertical-align:baseline;
+    background:transparent;
 }
 
-#nav {
-  padding: 30px;
+body {
+    line-height:1;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+article,aside,details,figcaption,figure,
+footer,header,hgroup,menu,nav,section {
+    display:block;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+nav ul {
+    list-style:none;
+}
+
+blockquote, q {
+    quotes:none;
+}
+
+blockquote:before, blockquote:after,
+q:before, q:after {
+    content:'';
+    content:none;
+}
+
+a {
+    margin:0;
+    padding:0;
+    font-size:100%;
+    vertical-align:baseline;
+    background:transparent;
+}
+
+/* change colours to suit your needs */
+ins {
+    background-color:#ff9;
+    color:#000;
+    text-decoration:none;
+}
+
+/* change colours to suit your needs */
+mark {
+    background-color:#ff9;
+    color:#000;
+    font-style:italic;
+    font-weight:bold;
+}
+
+del {
+    text-decoration: line-through;
+}
+
+abbr[title], dfn[title] {
+    border-bottom:1px dotted;
+    cursor:help;
+}
+
+table {
+    border-collapse:collapse;
+    border-spacing:0;
+}
+
+/* change border colour to suit your needs */
+hr {
+    display:block;
+    height:1px;
+    border:0;  
+    border-top:1px solid #cccccc;
+    margin:1em 0;
+    padding:0;
+}
+
+input, select {
+    vertical-align:middle;
+}
+
+html {
+  background-color: #15202b;
+}
+* {
+  color: white;
+  font-family: "Noto Sans JP";
 }
 </style>
---
+++ src/views/Login.vue
@@ -0,0 +1,79 @@
+<template>
+ <div>
+   <div class="header flex">
+     <div class="left">
+       <img class="logo" src="../../img/Logo.png" alt />
+     </div>
+     <div class="right flex">
+       <p>新規登録</p>
+       <p>ログイン</p>
+     </div>
+   </div>
+   <div class="card">
+     <p>ログイン</p>
+     <div class="form">
+       <input placeholder="メールアドレス" type="email" />
+       <input placeholder="パスワード" type="password" />
+       <button>ログイン</button>
+     </div>
+   </div>
+ </div>
+</template>
+export default {
+  name: 'Home',
+}
+<style scoped>
+.right {
+  align-items: center;
+}
+.right p {
+  margin-right: 20px;
+  cursor: pointer;
+}
+.header {
+  margin: 20px;
+}
+.logo {
+  width: 150px;
+  cursor: pointer;
+}
+.flex {
+  display: flex;
+  justify-content: space-between;
+}
+button {
+  width: 100px;
+  text-align: center;
+  padding: 8px 0 10px;
+  color: #fff;
+  background-color: #5419da;
+  border-radius: 25px;
+  cursor: pointer;
+}
+.card {
+  margin: 100px auto;
+  width: 350px;
+  background: #fff;
+  border-radius: 5px;
+  padding: 20px;
+}
+.card p {
+  color: black;
+  font-weight: bold;
+  text-align: center;
+}
+input {
+  margin-top: 15px;
+  width: 80%;
+  border-radius: 10px;
+  padding: 10px;
+  border: 1px solid black;
+  color: black;
+}
+.form {
+  text-align: center;
+}
+.form button {
+  margin-top: 15px;
+}
+</style>
